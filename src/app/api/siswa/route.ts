4f43d3cc9c37c6acdeb38db1475ab9ebc3d46ea3
Marwan <marwahpinrang6@gmail.com>
2024-10-01 13:59:10 +0800

    git init

---
+++ src/app/api/siswa/route.ts
@@ -0,0 +1,16 @@
+import { getBooks } from "@/app/libs/books_action";
+import { type NextRequest } from "next/server"
+
+export async function GET(request: NextRequest){
+    try{const searchParams = request.nextUrl.searchParams;
+        const data = await getBooks()
+        return Response.json(data)
+    }catch(error){
+        if(error instanceof Error){
+            console.error(error, " error in route")
+            return error.message
+        }else{
+            console.error("unexpected error accoured")
+        }
+    }
+}
--- src/app/components/Books.tsx
+++ src/app/components/Books.tsx
@@ -3,37 +3,46 @@
 import { useEffect, useState } from "react";
 import { createDataByInterval, getBooks } from "../libs/books_action";
 import { GetBooks } from "../libs/typeObject";
+import useSWR from "swr";
 
 export default function Books() {
-  const [datas, setDatas] = useState<GetBooks>();
+  // const [datas, setDatas] = useState<GetBooks>();
 
-  async function getAllBooks() {
-    const datas: GetBooks = await getBooks();
-    setDatas(datas);
-  }
+  // async function getAllBooks() {
+  //   const datas: GetBooks = await getBooks();
+  //   setDatas(datas);
+  // }
 
-  function interval() {
-    setInterval(async () => {
-      await createDataByInterval();
-    }, 3600000);
-  }
+  // function interval() {
+  //   setInterval(async () => {
+  //     await createDataByInterval();
+  //   }, 3600000);
+  // }
+
+  const fetcher = () => fetch("/api/siswa").then(res => res.json())
 
   async function handleUpdate() {
     await createDataByInterval();
   }
 
-  useEffect(() => {
-    getAllBooks().then((res) => res);
-    interval();
-  }, []);
-  return (
-    <>
-      <button onClick={handleUpdate}>update</button>
-      {!datas ? (
-        <div>Kosong</div>
-      ) : (
-        datas?.data?.map((item, index) => <p key={index}>{item.name}</p>)
-      )}
-    </>
-  );
+  // useEffect(() => {
+  //   getAllBooks().then((res) => res);
+  //   interval();
+  // }, []);
+  const { data, error, isLoading } = useSWR('/api/siswa', fetcher)
+ 
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+  console.log(data)
+  // render data
+  return <div>
+    <button onClick={handleUpdate}>update</button>
+    {
+      data.data.map((item: {id: number, name:string}, index: number)=>{
+        return(
+          <p key={index}>{item.name}</p>
+        )
+      })
+    }
+    </div>
 }
